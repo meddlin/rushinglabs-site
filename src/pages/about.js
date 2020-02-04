fc5d7a1c5f3ddbd8b716114a3a9ae43c0fd68476
meddlin <drushing.dev@gmail.com>
2020-02-03 23:46:24 -0600

    Paginated blog post list makes use of PostLink now.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { Link } from "gatsby";
 import styled from "styled-components";
-
 import Layout from "../components/layout";
 import Bio from "../components/Bio";
 import SEO from "../components/seo";
--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -1,45 +1,50 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
+import PostLink from "../components/post-link";
 import PaginationButtons from "../components/PaginationButtons";
 
 export default class BlogList extends React.Component {
-    render() {
-        const posts = this.props.data.allMarkdownRemark.edges;
-        const { currentPage, numPages } = this.props.pageContext;
-
-        return (
-            <Layout>
-                {posts.map( ({ node }) => {
-                    const title = node.frontmatter.title || node.fields.slug;
-                    return (
-                        <div key={node.fields.slug}>{title}</div>
-                    );
-                })}
-
-                <PaginationButtons currentPage={currentPage} numPages={numPages} />
-            </Layout>
-        );
-    }
+	render() {
+		const posts = this.props.data.allMarkdownRemark.edges;
+		const { currentPage, numPages } = this.props.pageContext;
+		
+		return (
+			<Layout>
+				{posts.map( ({ node }) => {
+					// const title = node.frontmatter.title || node.fields.slug;
+					return (
+						<PostLink key={node.fields.slug} post={node} />
+					);
+				})}
+				
+				<PaginationButtons currentPage={currentPage} numPages={numPages} />
+			</Layout>
+		);
+	}
 };
-
+		
 export const blogListQuery = graphql`
 query blogListQuery($skip: Int!, $limit: Int!) {
-  allMarkdownRemark(
-    sort: { fields: [frontmatter___date], order: DESC }
-    limit: $limit
-    skip: $skip
-  ) {
-    edges {
-      node {
-        fields {
-          slug
-        }
-        frontmatter {
-          title
-        }
-      }
-    }
-  }
-}
-`
+	allMarkdownRemark(
+		sort: { fields: [frontmatter___date], order: DESC }
+		limit: $limit
+		skip: $skip
+		) {
+			edges {
+				node {
+					excerpt(pruneLength: 250)
+					fields {
+						slug
+					}
+					frontmatter {
+						date(formatString: "MMMM DD, YYYY")
+						path
+						title
+						tags
+					}
+				}
+			}
+		}
+	}
+`;
