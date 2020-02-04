a9010d733f72992ef0eadeae827f0cc0fb53c4ce
meddlin <drushing.dev@gmail.com>
2020-02-04 00:54:53 -0600

    Layout and styles applied to blog post template.

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,19 +1,39 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import styled from "styled-components";
+
+const PostContainer = styled.div`
+	margin: 0 8em 0 8em;
+`;
+const PostDate = styled.p`
+	font-size: 0.83255rem;
+	line-height: 1.75rem;
+	display: block;
+	margin-bottom: 1.75rem;
+	margin-top: -1.75rem;
+`;
+const PostSeparator = styled.hr`
+	margin-bottom: 1.75rem;
+`;
 
 export default function Template({ data }) { // this prop will be injected by the GraphQL query below.
     const { markdownRemark } = data // data.markdownRemark holds your post data
-    const { frontmatter, html } = markdownRemark;
+    const { frontmatter, html, excerpt } = markdownRemark;
 
     return (
-        <div className="blog-post-container">
-            <div className="blog-post">
+        <Layout>
+            <SEO title={frontmatter.title} description={excerpt} />
+
+            <PostContainer>
                 <h1>{frontmatter.title}</h1>
-                <h2>{frontmatter.date}</h2>
-                <div className="blog-post-content" 
-                    dangerouslySetInnerHTML={{ __html: html }} />
-            </div>
-        </div>
+                <PostDate>{frontmatter.date}</PostDate>
+                <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
+            
+                <PostSeparator />
+            </PostContainer>
+        </Layout>
     );
 };
 
@@ -21,6 +41,7 @@ export const pageQuery = graphql`
 query($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
         html
+        excerpt(pruneLength: 250)
         frontmatter {
             date(formatString: "MMMM DD, YYYY")
             path
