9fb37e071de4da8541de1ae68a69b8473c515af9
meddlin <drushing.dev@gmail.com>
2020-02-04 00:35:47 -0600

    Changed header to styled-components and changed colors.

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,34 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+	background: darkgray;
+	margin-bottom: 1.45rem;
+	font-family: 'Source Sans Pro', sans-serif;
+
+	div {
+		margin: 0 auto;
+		max-width: 960px;
+		padding: 0.85rem 1.0875rem;
+	}
+`;
+
+const HeaderText = styled.h3`
+	font-family: inherit;
+	margin: 0;
+`;
 
 const Header = ({ siteTitle }) => (
-	<header style={{ background: `rebeccapurple`, marginBottom: `1.45rem`, }}>
-	
-		<div style={{
-				margin: `0 auto`,
-				maxWidth: 960, padding: `1.45rem 1.0875rem`, }}>
-			<h1 style={{ margin: 0 }}>
-				<Link to="/"
-					style={{
-						color: `white`,
-						textDecoration: `none`,
-					}}>
-					{siteTitle}
-				</Link>
-			</h1>
+	<HeaderContainer>
+		<link href="https://fonts.googleapis.com/css?family=Muli|Source+Sans+Pro&display=swap" rel="stylesheet" />
+		<div>
+			<HeaderText>
+				<Link to="/" style={{ color: `white`, textDecoration: `none` }}>{siteTitle}</Link>
+			</HeaderText>
 		</div>
-	</header>
+	</HeaderContainer>
 )
 	
 Header.propTypes = {
